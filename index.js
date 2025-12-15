6ec6db019b303218e8761e9512bc2131c2090821
Ayush Kumar <ayushkumarwk555@gmail.com>
2025-12-15 18:41:49 +0530

    Fixed Path for Static Files

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import cors from "cors";
 import quotesRouter from "./routes/quotes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express(); // Initializing App Object
 const port = process.env.PORT || 8000;
+
+// Dirname for public folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // CORS
 app.use(cors());
 
 // Static Files
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/quotes", quotesRouter);
